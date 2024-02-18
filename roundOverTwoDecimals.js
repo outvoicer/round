7a6a40187a814b54a382b9e3b894abfb298448aa
martinsookael <martinsookael@gmail.com>
2024-02-18 18:16:37 +0200

    firstDecimal to first etc

--- roundOverTwoDecimals.js
+++ roundOverTwoDecimals.js
@@ -1,26 +1,26 @@
 
 'use strict'
 
-module.exports = function roundThreeDecimals (entry, comma, integer) {
-  // HANDLE NUMBER NUMBER, THAT HAS OVER TWO DECIMALS
+module.exports = function roundOverTwoDecimals (entry, comma, integer) {
+  // HANDLE NUMBER, THAT HAS OVER TWO DECIMALS AFTER COMMA
   // ON ERROR RETURN -0.00
   let output = '-0.00'
   // MAP THE INPUT
-  , firstDecimal = entry[comma + 1]
-  , secondDecimal = entry[comma + 2]
+  , first = entry[comma + 1]
+  , second = entry[comma + 2]
   // THIRD DECIMAL, OUR ONLY CONSTANT
-  const thirdDecimal = entry[comma + 3]
-  if (thirdDecimal >= 5) {
+  const third = entry[comma + 3]
+  if (third >= 5) {
     // IF THE THIRD DECIMAL IS 5 OR BIGGER, ADD + 1 TO SECOND DECIMAL
-    secondDecimal = Number(secondDecimal) + 1
-    if (secondDecimal === 10) {
+    second = Number(second) + 1
+    if (second === 10) {
       // IF SECOND DECIMAL BECAME 10, MAKE IT A ZERO AND CALL THE MANAGER
-      secondDecimal = 0
-      firstDecimal = Number(firstDecimal) + 1
-      if (firstDecimal === 10) {
+      second = 0
+      first = Number(first) + 1
+      if (first === 10) {
         // IF THE FIRST DECIMAL BECAME 10, MAKE IT A ZERO AND INCREASE THE MAIN NUMBER
-        firstDecimal = 0
-        // IF DOES NOT START WITH -, EXPECT INPUT TO BE POSITIVE NUMBER
+        first = 0
+        // IS NUMBER POSITIVE OR NEGATIVE
         const positive = entry.indexOf('-') !== 0
         if (positive) {
           // IF IT'S POSITIVE, COUNT UP
@@ -33,6 +33,6 @@ module.exports = function roundThreeDecimals (entry, comma, integer) {
     }
   }
   // COMPOSE THE ROUNDED NUMBER
-  output = String(integer) + '.' + firstDecimal + secondDecimal
+  output = String(integer) + '.' + first + second
   return output
 }
