58262d0b8158cad6bc4c175841c31a8741db5219
martinsookael <martinsookael@gmail.com>
2024-02-20 19:51:18 +0200

    limit input to String and Number, exclude support for ",265" because of unexpected existance of "-,265".

--- index.js
+++ index.js
@@ -3,23 +3,31 @@
 
 const overTwoDecimals = require('./overTwoDecimals')
 
-module.exports = function round(input, secondParamater) {
-  if (secondParamater) {
-    // HANDLE PURE COMMA INPUT round(0,05)
+module.exports = function round(input, secondParam) {
+  /*
+    ERROR HANDLING
+  */
+  if (secondParam) {
+    // HANDLE PURE COMMA INPUT ERROR LIKE round(0,05)
+    return '-0.00'
+  }
+  // MAKE SURE 0 IS NOT HANDLED AS false
+  if (input === 0) {
+    input = '0.00'
+  }
+  // MAKE SURE TYPE IS STRING OR NUMBER
+  const type = typeof input
+  if (type !== 'string' && type !== 'number') {
     return '-0.00'
   }
   /*
     PPEPARE input
   */
   let output = '-0.00'
-  // MAKE SURE 0 IS NOT HANDLED AS false
-  if (input === 0) {
-    input = '0.00'
-  }
   // MAKE INPUT TO A STRING, TRIM IT AND REPLACE ',' WITH '.' JUST IN CASE
   const entry = String(input).trim().replace(',', '.')
   if (isNaN(entry)) {
-    // AND RETURN WITH "ERROR" IF IT AIN'T A NUMBER
+    // IF IT'S STILL NOT A NUMBER, THEN RETURN ERROR
     return '-0.00'
   }
   // FIND COMMA POSITION
@@ -28,18 +36,20 @@ module.exports = function round(input, secondParamater) {
   let integer = entry.substring(0, comma)
   let zero = ''
   if (comma > -1) {
+
     // COMMA EXISTS
     if (comma === 0) {
-      // BUT THERE IS NO NUMBER BEFORE COMMA (,254)
-      // WE ASSIME IT'S A ZERO
-      integer = '0'
-      // FOR CASES WHEN INTEGER IS NOT USED:
-      zero = '0'
+      // COMMA IS FIRST ERROR round('.265')
+      return '-0.00'
+    }
+    if (comma === 1 && integer === '-') {
+      // COMMA IS FIRST NEGATIVE NUMBER ERROR round('-.265')
+      return '-0.00'
     }
     /*
       PERFORM ROUNDING
     */
-    // BUT HOW MANY DECIMALS DO WE HAVE?
+    // HOW MANY DECIMALS DO WE HAVE AFTER COMMA?
     const decimals = entry.length - comma - 1
     if (decimals > 2) {
       // HAS OVER TWO DECIMALS
